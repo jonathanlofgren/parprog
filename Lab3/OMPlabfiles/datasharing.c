e2e280340f4b2049c4ee849097daf4f2327dd79b
Jonathan Lofgren <lofgren021@gmail.com>
2015-02-25 14:14:20 +0100

    Lab3 stuff

--- Lab3/OMPlabfiles/datasharing.c
+++ Lab3/OMPlabfiles/datasharing.c
@@ -8,17 +8,17 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  int a,b;
-  a=100;b=10;
-  a = 40;
+    int a,b;
+    a=100;b=10;
+    a = 40;
  
 #pragma omp parallel firstprivate(a)
-  {
-    a+=b;
-    printf("a= %d b= %d\n",a,b);
-  }
+    {
+	a+=b;
+	printf("a= %d b= %d\n",a,b);
+    }
 
-  printf("a = %d", a);
+    printf("a = %d", a);
 
-  return 0;
+    return 0;
 }
--- Lab3/OMPlabfiles/enumsort2.c
+++ Lab3/OMPlabfiles/enumsort2.c
@@ -11,12 +11,14 @@ int timer(void);
 
 int main(int argc, char *argv[]) {
 
-    int len=50000,seed,i,j,rank;
+    int len=50000,seed,i,j;
     double *indata, *outdata,wtime;
   
     indata=(double *)malloc(len*sizeof(double));
     outdata=(double *)malloc(len*sizeof(double));
 
+    omp_set_nested(1);
+    printf("Nested: %d\n", omp_get_nested());
 
     // Generate random numbers
     for (i=0;i<len;i++){
@@ -28,20 +30,22 @@ int main(int argc, char *argv[]) {
 
     wtime=omp_get_wtime();
 
+    int rank = 0;
 
-#pragma omp parallel for private(rank)
+#pragma omp parallel for firstprivate(rank)
     for (j=0;j<len;j++)
     {
-	rank=0;
-	//printf("number: %d reporting in\n", omp_get_thread_num());
 #pragma omp parallel for reduction(+:rank)
 	for (i=0;i<len;i++)
 	{
+	    //printf("Id: %d, %d, %d\n", omp_get_thread_num(),i,j);
+	    
 	    if (indata[i]<indata[j]) rank++;
-	    printf("number: %d reporting in\n", omp_get_thread_num());
 	}
-	outdata[rank]=indata[j];
+    	outdata[rank]=indata[j];
+	rank=0;
     }
+
 	
     wtime=omp_get_wtime()-wtime;
     printf("Time: %f sec \n",wtime);
