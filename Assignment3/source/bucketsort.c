8cd9c7bcd0dd2e55f1158270ea3e80a66d0c7118
Jonathan Lofgren <lofgren021@gmail.com>
2015-02-27 16:43:53 +0100

    Just small changes

--- Assignment3/source/bucketsort.c
+++ Assignment3/source/bucketsort.c
@@ -13,10 +13,13 @@
 
 #define PI 3.14159265358979323846
 
-//#define PRINT
 
-
-void bucket_balance(bucket *blist, int bucket_count, bucketbucket *bb, int num_threads)
+/* Distributes the buckets in blist into groups in bb 
+ * to balance the total work required to sort 
+ * each group of buckets.
+ */
+void bucket_balance(bucket *blist, int bucket_count, 
+		    bucketbucket *bb, int num_threads)
 {
     bucket *sorted_buckets = (bucket *)malloc(bucket_count*sizeof(bucket));
     memcpy(sorted_buckets, blist, bucket_count*sizeof(bucket)); 
@@ -28,7 +31,7 @@ void bucket_balance(bucket *blist, int bucket_count, bucketbucket *bb, int num_t
 
     for (i = bucket_count - 1; i >= 0; i--)
     {
-	// Get work load for this bucket
+	// estimate work with n*log n
 	if (sorted_buckets[i].count != 0)
 	{
 	    load = sorted_buckets[i].count*log(sorted_buckets[i].count);
@@ -50,22 +53,22 @@ void bucket_balance(bucket *blist, int bucket_count, bucketbucket *bb, int num_t
 	bb[least].buckets[bb[least].bucket_count] = sorted_buckets[i];
 	bb[least].load += load;
 	bb[least].bucket_count++;
-
-
-	//printf("Count: %d\n", sorted_buckets[i].count);
-	
     }
     
     printf("\nLoad balancing:\n");
     for (i = 0; i < num_threads; i++)
     {
-	printf("Load: %0.2f\n", bb[i].load);
+	printf("Bucket %d: %0.2f\n", i, bb[i].load);
     }
     printf("\n");
 
     free(sorted_buckets);
 }
 
+
+/* Allocate num bucketbuckets with each with
+ * room for bucket_count buckets
+ */
 bucketbucket *create_bucketbucket(int num, int bucket_count)
 {
     bucketbucket *bb = (bucketbucket *)malloc(num*sizeof(bucketbucket));
@@ -81,6 +84,9 @@ bucketbucket *create_bucketbucket(int num, int bucket_count)
     return bb;
 }
 
+
+/* Frees the bucketbuckets and all buckets it contains 
+ */
 void free_bucketbucket(bucketbucket *bb, int num)
 {
     int i;
@@ -92,13 +98,125 @@ void free_bucketbucket(bucketbucket *bb, int num)
     free(bb);
 }
 
+
+/* Fills the array a with n normal distributed 
+ * random numbers
+ */
+void fill_normal_dist(double *a, int n)
+{
+    srand48(time(NULL));
+    double u1, u2;
+    int i;
+
+    for (i = 0; i < n; i+=2)
+    {
+	u1 = drand48();
+	u2 = drand48();
+	a[i] = sqrt(-2.0*log(u1))*cos(2*PI*u2);
+	a[i+1] = sqrt(-2.0*log(u1))*sin(2*PI*u2);
+    } 
+}
+
+
+/* Sort the buckets in buckets by manually balancing
+ * the buckets into groups and sorting each group of buckets
+ * on a separate thread. 
+ */
+void sort_manual_loadbalance(bucket *buckets, int bucket_count,
+			     int num_threads)
+{
+    bucketbucket *bb = create_bucketbucket(num_threads, bucket_count);
+    bucket_balance(buckets, bucket_count, bb, num_threads);
+    
+    printf("Timings:\n");
+
+#pragma omp parallel num_threads(num_threads)
+    {
+	int startime = omp_get_wtime();
+	int tid = omp_get_thread_num();
+	int i;
+	for (i = 0; i < bb[tid].bucket_count; i++)
+	{
+	    quicksort(bb[tid].buckets[i].b, 0,bb[tid].buckets[i].count - 1);
+	}
+
+	printf("Thread %d: %0.5f sec\n", tid, omp_get_wtime()-startime);
+    }
+
+    printf("\n");
+    free_bucketbucket(bb, num_threads);
+}
+
+
+/* Sort the buckets by creating a task for each sort
+ * and letting openmp handle the load balancing
+ */
+void sort_tasks(bucket *buckets, int bucket_count)
+{
+#pragma omp parallel
+    {
+#pragma omp single
+	{
+	    int i;
+	    for (i = 0; i < bucket_count; i++)
+	    {
+#pragma omp task firstprivate(i)
+		{
+		    quicksort(buckets[i].b, 0, buckets[i].count - 1);
+		}	    
+	    }
+	}
+    }   
+}
+
+
+/* Sort the buckets serially 
+ */
+void sort_serial(bucket *buckets, int bucket_count)
+{
+    int i;
+    for (i = 0; i < bucket_count; i++)
+    {
+	quicksort(buckets[i].b, 0, buckets[i].count - 1);    
+    } 
+}
+
+
+/* Sort the buckets by creating a separate thread 
+ * for each buckets and letting the run time system
+ * handle the work load.
+ */
+void sort_run_time_system(bucket *buckets, int bucket_count)
+{
+    int tid;
+#pragma omp parallel private(tid) num_threads(bucket_count)
+    {
+	tid = omp_get_thread_num();
+	quicksort(buckets[tid].b, 0, buckets[tid].count - 1);    
+    }   
+}
+
+
+/* Sort the buckets in parallel for loop and let
+ * openmp do the load balancing with the schedule directive.
+ */
+void sort_omp_scheduling(bucket *buckets, int bucket_count)
+{
+    int i;
+#pragma omp parallel for schedule(dynamic,1)
+    for (i = 0; i < bucket_count; i++)
+    {
+	quicksort(buckets[i].b, 0, buckets[i].count - 1);
+
+    } 
+}
+
+
 int main(int argc, char *argv[]) 
 {
-    omp_set_num_threads(4);
+    //omp_set_num_threads(4);
     int num_threads = omp_get_max_threads();
     printf("Threads = %d\n", num_threads);
-
-  
 
     if (argc != 3)
     {
@@ -111,34 +229,16 @@ int main(int argc, char *argv[])
     int bucket_size = 2*n/bucket_count;
 
     double *a = (double *)malloc(n*sizeof(double));
-
-    int i;
-    srand48(time(NULL));
-    /*
-    for (i = 0; i < n; i++)
-    {
-	a[i] = drand48();
-    }
-    */
-    
-    // normal distribution
-    double u1, u2;
-    for (i = 0; i < n; i+=2)
-    {
-	u1 = drand48();
-	u2 = drand48();
-	a[i] = sqrt(-2.0*log(u1))*cos(2*PI*u2);
-	a[i+1] = sqrt(-2.0*log(u1))*sin(2*PI*u2);
-    }
+    fill_normal_dist(a, n);
 
     // start timing here!
     double timer1, start;
     timer1=omp_get_wtime();
     start = timer1;
 
-    // allocate the buckets
+    // Allocate the buckets
     bucket *blist = (bucket *)malloc(bucket_count*sizeof(bucket));
-
+    int i;
     for (i = 0; i < bucket_count; i++)
     {
 	blist[i].b = (double *)malloc(bucket_size*sizeof(double));
@@ -151,7 +251,8 @@ int main(int argc, char *argv[])
 
     int pos, count;
     double offset = 3, temp;
-    // assign the elements to the buckets
+
+    // Assign the elements to the buckets
     for (i = 0; i < n; i++)
     {
 	temp = a[i]+offset;
@@ -172,7 +273,6 @@ int main(int argc, char *argv[])
 	    memcpy(bnew, blist[pos].b, count*sizeof(double));
 	    free(blist[pos].b);
 	    blist[pos].b = bnew;
-
 	}
 
 	blist[pos].b[count] = a[i];
@@ -181,102 +281,25 @@ int main(int argc, char *argv[])
 
     printf("Assigning to bucket time: %0.4f sec\n", omp_get_wtime()-timer1);
     timer1=omp_get_wtime();
-
-#ifdef PRINT
-    print(a, n);
-    for (i = 0; i < bucket_count; i++)
-    {
-	printf("size: %d, count: %d ## ", blist[i].size, blist[i].count);
-	print(blist[i].b, blist[i].count);
-    }
-#endif
     
-/* Manual load balancing */
-    
-    bucketbucket *bb = create_bucketbucket(num_threads, bucket_count);
-    bucket_balance(blist, bucket_count, bb, num_threads);
-    
-    int tid;
-#pragma omp parallel private(i, tid) num_threads(num_threads)
-    {
-	int startime = omp_get_wtime();
-	tid = omp_get_thread_num();
+    /* Manual load balancing */
+    sort_manual_loadbalance(blist, bucket_count, num_threads);
 
-	for (i = 0; i < bb[tid].bucket_count; i++)
-	{
-	    quicksort(bb[tid].buckets[i].b, 0,bb[tid].buckets[i].count - 1);
-	}
+    /* Serial */
+    //sort_serial(blist, bucket_count);
 
-	printf("Tid: %0.5f\n", omp_get_wtime()-startime);
-    }
+    /* One thread for each bucket */
+    //sort_run_time_system(blist, bucket_count);
 
-      free_bucketbucket(bb, num_threads);
-/**/
+    /* Using tasks */
+    //sort_tasks(blist, bucket_count);
 
-/* Serial *
-    for (i = 0; i < bucket_count; i++)
-    {
-	quicksort(blist[i].b, 0, blist[i].count - 1);    
-    }
-/**/
+    /* Dynamic scheduling */
+    //sort_omp_scheduling(blist, bucket_count);
 
-/* Static parallel for *
-#pragma omp parallel for
-    for (i = 0; i < bucket_count; i++)
-    {
-	quicksort(blist[i].b, 0, blist[i].count - 1);
-
-    }
-/**/
-
-  
-/* One thread for each bucket *
-    int tid;
-#pragma omp parallel private(tid) num_threads(bucket_count)
-    {
-	tid = omp_get_thread_num();
-	quicksort(blist[tid].b, 0, blist[tid].count - 1);    
-    }
-/**/
-
-
-/* Using tasks *
-#pragma omp parallel
-    {
-#pragma omp single
-	{
-	    for (i = 0; i < bucket_count; i++)
-	    {
-#pragma omp task firstprivate(i)
-		{
-		    quicksort(blist[i].b, 0, blist[i].count - 1);
-		}	    
-	    }
-	}
-    }
-/**/
-
-
-/* Dynamic scheduling *
-#pragma omp parallel for schedule(dynamic,1)
-    for (i = 0; i < bucket_count; i++)
-    {
-	quicksort(blist[i].b, 0, blist[i].count - 1);
-
-    }
-/**/
-
-    printf("Quicksort buckets time: %0.4f sec\n", omp_get_wtime()-timer1);
+    printf("## Quicksort buckets time: %0.4f sec\n", 
+	   omp_get_wtime()-timer1);
     timer1=omp_get_wtime();
-
-#ifdef PRINT
-    printf("Sorted buckets: \n");
-    for (i = 0; i < bucket_count; i++)
-    {
-	printf("size: %d, count: %d ## ", blist[i].size, blist[i].count);
-	print(blist[i].b, blist[i].count);
-    }
-#endif
 
     // copy the sorted buckets back to a
     int apos = 0;
@@ -290,12 +313,8 @@ int main(int argc, char *argv[])
     timer1=omp_get_wtime();
 
     timer1=timer1-start;
-    printf("Toal time: %f sec\n",timer1);
+    printf("Total time: %f sec\n",timer1);
 
-#ifdef PRINT
-    printf("Sorted a: ");
-    print(a, n);
-#endif
 
     // check that the array is sorted
     for (i = 0; i < n-1; i++)
@@ -306,16 +325,42 @@ int main(int argc, char *argv[])
 	    break;
 	}
     }
-
     if (i == n-1)
 	printf("Pengar på fredag!\n");
 
     // Clean up!
     for (i = 0; i < bucket_count; i++)
 	free(blist[i].b);
-
     free(a);
-  
     free(blist);
 
+
+    /*
+    n = 10000000;
+    a = (double *)malloc(n*sizeof(double));
+    for (i=0; i<n; i++)
+    {
+	a[i] = drand48();
+    }
+
+    printf("\nSort testing:\n\n");
+
+    timer1=omp_get_wtime();
+    quicksort_cutoff(a, 0, n-1);
+    printf("Sort test time: %0.4f sec\n", omp_get_wtime()-timer1);
+
+    // check that the array is sorted
+    for (i = 0; i < n-1; i++)
+    {
+	if (a[i] > a[i+1])
+	{
+	    printf("Error at position %d.\n", i);
+	    break;
+	}
+    }
+    if (i == n-1)
+	printf("Pengar på fredag!\n");
+    */
+	
 }
+
--- Assignment3/source/quicksort.c
+++ Assignment3/source/quicksort.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include "bucketsort.h"
 
+#define SWAP(X, Y, TEMP) {TEMP = X; X = Y; Y = TEMP;}
+
 void swap(double *a, double *b, double *temp);
 void print(double *a, int n);
 void quicksort(double *a, int p, int r);
@@ -22,6 +24,57 @@ void swap(double *a, double *b, double *temp)
     *temp = *b;
     *b = *a;
     *a = *temp;
+}
+
+void insertion_sort(double *a, int l, int r)
+{
+    int i = l+1, j;
+    double temp;
+
+    while (i <= r)
+    {
+	j = i;
+
+	while (j > 0 && a[j] < a[j-1]) 
+	{
+	    SWAP(a[j-1], a[j], temp)
+	    j--;
+	}
+	
+	i++;
+    }
+}
+
+void quicksort_cutoff(double *a, int p, int r)
+{
+    if (r-p < 15)
+    {
+	insertion_sort(a, p, r);
+    }
+    else
+    {
+	double pivot = a[r];
+	int i = p - 1;
+	int j = p;
+	double temp;
+
+	while (j < r)
+	{
+	    if (a[j] <= pivot)
+	    {
+		i++;
+		swap(&a[i], &a[j], &temp);
+	    }
+	
+	    j++;
+	}
+
+	i++;
+	swap(&a[r], &a[i], &temp);
+
+	quicksort(a, p, i-1);
+	quicksort(a, i+1, r);
+    }
 }
 
 void quicksort(double *a, int p, int r)
@@ -59,6 +112,8 @@ void swap_bucket(bucket *a, bucket *b, bucket *temp)
     *a = *temp;
 }
 
+/* Sorts an array of buckets according to their count
+ */
 void quicksort_bucket(bucket *blist, int p, int r)
 {
     if (p < r)
